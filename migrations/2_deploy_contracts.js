65089b65ea82a79d0b894bf9bf725813f0f958a3
yongdk1 <yongdk1@gmai.com>
2022-09-29 17:26:30 +0800

    ignore json files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,7 +9,7 @@ module.exports = async function (deployer, _network, addresses) {
   const [admin, oracle, gambler1, gambler2, gambler3, gambler4, _] = addresses;
   await deployer.deploy(PredictionMarket, oracle);
   const predictionMarket = await PredictionMarket.deployed();
-  /* await predictionMarket.placeBet(
+  await predictionMarket.placeBet(
     Side.Biden, 
     {from: gambler1, value: web3.utils.toWei('1')}
   );
@@ -24,5 +24,5 @@ module.exports = async function (deployer, _network, addresses) {
   await predictionMarket.placeBet(
     Side.Trump, 
     {from: gambler4, value: web3.utils.toWei('1')}
-  );*/
+  );
 };
