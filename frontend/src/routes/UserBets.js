a45539b0ebf713683740741bb990cb4e8f6f01c1
pogoballz <graezt@gmail.com>
2022-10-17 17:33:38 +0800

    minor changes

--- frontend/src/routes/UserBets.js
+++ frontend/src/routes/UserBets.js
@@ -1,19 +1,37 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 
+function UserBets(props) {
+  const [userBets, setUserBets] = useState(undefined);
+  //   const userBets = async () => {
+  //     try {
+  //       await props.predictionMarket.getUserBets(props.signer);
+  //     } catch (err) {
+  //       console.log(err);
+  //       alert("Unable to retrieve user bets!");
+  //     }
+  //   };
+  //     useEffect(() => {
+  //     const userBets = async () = {
+  //         // const getUserBets = await props.predictionMarket.getUserBets(props.signer);
+  //         // const topics = await predictionMarket.getUserBets();
+  //         setUserBets(getUserBets);
 
-function UserBets(props){
+  //     };userBets();
 
-    const bets = props.userBets();
-    console.log("USER BETS:", bets)
+  //   },[])
+  useEffect(() => {
+    (async () => {
+      const users = props.predictionMarket.getUserBets(props.signer);
+      setUserBets(users);
+    })});
 
-    return(
-        <div className="parent-container">
-            <h2 className="addHeader">
-                List of Bets that you have placed:
-            </h2>
-        </div>
-    )
+  console.log("USER BETS:", userBets);
+
+  return (
+    <div className="parent-container">
+      <h2 className="addHeader">List of Bets that you have placed:</h2>
+    </div>
+  );
 }
-
 
 export default UserBets;
