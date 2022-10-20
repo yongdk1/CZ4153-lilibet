62d22e6eeaa0fbab547e0bf32550e64833e9af59
pogoballz <graezt@gmail.com>
2022-10-20 15:06:11 +0800

    changes to userBet

--- frontend/src/routes/UserBets.js
+++ frontend/src/routes/UserBets.js
@@ -2,28 +2,63 @@ import React, { useState, useEffect } from "react";
 
 function UserBets(props) {
   const [userBets, setUserBets] = useState(undefined);
-  //     useEffect(() => {
-  //     const userBets = async () = {
-  //         // const getUserBets = await props.predictionMarket.getUserBets(props.signer);
-  //         // const topics = await predictionMarket.getUserBets();
-  //         setUserBets(getUserBets);
 
-  //     };userBets();
+  const sampleBets = [
+    {
+      name: "yayaya",
+      user: "me",
+      side: "biden",
+      amt: "1000",
+      claimed: false,
+    },
+    {
+      name: "papaya",
+      user: "me",
+      side: "trump",
+      amt: "2000",
+      claimed: false,
+    },
+  ];
 
-  //   },[])
   useEffect(() => {
     const init = async () => {
-      const bets = await props.predictionMarket.getUserBets(props.signer);
+      let bets = await props.predictionMarket.getUserBets(props.signer);
+      bets = bets.map((x) =>
+        Object.fromEntries(Object.entries(x).filter(([k, v]) => isNaN(k)))
+      );
       setUserBets(bets);
     };
     init();
   }, []);
+
+  // bets = [{}, {}];
 
   console.log("USER BETS:", userBets);
 
   return (
     <div className="parent-container">
       <h2 className="addHeader">List of Bets that you have placed:</h2>
+
+      {sampleBets.map((bet, i) => {
+        return (
+          <div className="view-item">
+            <div className="topic-item">
+              <p>
+                <span className="question-attr">Topic Name:</span> &nbsp;
+                {bet.name}
+              </p>
+              <p>
+                <span className="question-attr">Bet Side:</span> &nbsp;
+                {bet.side}
+              </p>
+              <p>
+                <span className="question-attr">Bet Amount:</span> &nbsp;
+                {bet.amt}
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
